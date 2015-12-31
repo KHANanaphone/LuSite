4380a8b06919d267a96157ed379151b90226f2d9
Adan Ferguson <adanferguson@gmail.com>
2015-12-31 03:51:44 -0700

    Organizing the templating

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ var app = express();
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-// use res.render to load up an ejs view file
-
 // index page
 app.get('/', function(req, res) {
-    res.render('layout');
+    res.render('layout', {page: 'index.ejs'});
 });
+
+app.use(express.static(__dirname + '/assets'));
 
 app.listen(3001);
 console.log('listening on 3001');
